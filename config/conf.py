1deff9c8f7a27ac5892e1ceabba31953dd272ec6
ZHR-Harris <52810398+ZHR-Harris@users.noreply.github.com>
2021-02-24 00:56:06 +0800

    change ip to the master's ip

--- config/conf.py
+++ config/conf.py
@@ -1,19 +1,20 @@
 import os
 currentpath = os.path.dirname(os.path.realpath(__file__))
-project_basedir = os.path.join(currentpath,'..')
+project_basedir = os.path.join(currentpath, '..')
 
-distributed_datadir = os.path.join(project_basedir,'data/distributed')
-distributed_server_weight_dir = os.path.join(project_basedir,'data/prepare_weight')
-download_weight_dir = os.path.join(project_basedir,'data/download_weight')
+distributed_datadir = os.path.join(project_basedir, 'data/distributed')
+distributed_server_weight_dir = os.path.join(
+    project_basedir, 'data/prepare_weight')
+download_weight_dir = os.path.join(project_basedir, 'data/download_weight')
 
-validate_dir = os.path.join(project_basedir,'data/validate')
-daily_log_dir = os.path.join(project_basedir,'data/log_update')
-history_selfplay_dir = os.path.join(project_basedir,'data/history_selfplays')
-model_dir = os.path.join(project_basedir,'data/models')
-imsa_cbf = os.path.join(project_basedir,'data/imsa-cbf')
+validate_dir = os.path.join(project_basedir, 'data/validate')
+daily_log_dir = os.path.join(project_basedir, 'data/log_update')
+history_selfplay_dir = os.path.join(project_basedir, 'data/history_selfplays')
+model_dir = os.path.join(project_basedir, 'data/models')
+imsa_cbf = os.path.join(project_basedir, 'data/imsa-cbf')
 
 port = 10088
-server = 'http://10.109.247.219'
+server = 'http://139.198.16.150'
 
 train_lr = 0.01
 train_epoch = 1
@@ -38,4 +39,3 @@ val_temp_round = 12
 upload_gameplay_to_server = True
 verbose = False
 weight_up_immediately = True
-
